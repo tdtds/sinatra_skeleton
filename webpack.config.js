1eae11dce46fc3af5522dfd4324d1e9ac4d08ce7
TADA Tadashi <t@tdtds.jp>
2016-05-26 21:55:34 +0900

    split css and js to each files

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 const path = require('path');
 
 module.exports = {
@@ -5,26 +7,33 @@ module.exports = {
 		app: './assets/js/main.js'
 	},
 	output: {
-		path: path.join(__dirname, 'public/js'),
-		filename: '[name].js'
+		path: path.join(__dirname, 'public'),
+		filename: '/js/[name].js'
 	},
 	module: {
 		loaders: [
 			{
-				loader: 'style!css',
-				test: /\.css$/
-			},
-			{
-				loader: 'babel',
-				exclude: /node_modules/,
 				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
 				query: {
 					cacheDirectory: true,
 					presets: ['react', 'es2015']
 				}
+			},
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+			},
+			{
+				test: /\.(png|jpg)$/,
+				loader: "file-loader?name=/img/[name].[ext]"
 			}
 		]
 	},
+	plugins: [
+		new ExtractTextPlugin('/css/[name].css')
+	],
 	resolve: {
 		extensions: ['', '.js', '.css']
 	}

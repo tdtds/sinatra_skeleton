7494d5cd1e17de74bbdb83588027a15019481063
TADA Tadashi <t@tdtds.jp>
2016-06-03 11:52:40 +0900

    get initial data from server via Ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ import App from './container/app';
 
 require('../css/main.css');
 
-/* === HOT TO SET CSRF TOKEN ===
+/* === HOT TO SET CSRF TOKEN USING rack_csrf GEM ===
  *
 var csrf_token = document.querySelector('meta[name="_cstf"]').content;
 fetch(..., {X_CSRF_TOKEN: csrf_token})...;
@@ -38,5 +38,7 @@ app.on(":start-async-updating", () => {
 app.on(":end-async-updating", () => {
 });
 
-app.update(_initialState => ({count: 0}));
-
+fetch("/initial.json").
+then(response => response.json()).
+then(json => app.update(_initialState => (json))).
+catch(err => console.info(err));
